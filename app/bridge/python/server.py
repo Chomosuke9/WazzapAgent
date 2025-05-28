e4a569690f9f8da02e1cf4ed7c40a6a9fc9bbf58
Chomosuke9 <165505740+Chomosuke9@users.noreply.github.com>
2025-05-28 11:44:04 +0700

    Process message from client parallely

--- app/bridge/python/server.py
+++ app/bridge/python/server.py
@@ -12,6 +12,29 @@ async def handle_new_valid_client(websocket):
   await websocket.send(auth_data(status="success", token=token))
   print(clients)
 
+async def handle_websocket_message(message, websocket):
+      try:
+        msg = json.loads(message)
+      except json.JSONDecodeError:
+        await websocket.send(notify_data(content="Invalid JSON"))
+        return
+
+      if websocket in clients and msg.get("token") == clients[websocket]:
+        create_task(handle_message(msg))
+
+      elif websocket not in clients:
+        if msg.get("type") == "auth" and msg.get("key") == secret_key:
+          await websocket.send(notify_data(content="Authenticated Successfully, adding to verified clients..."))
+          await handle_new_valid_client(websocket)
+
+        else:
+          await websocket.send(notify_data(content="Unauthorized. Please authenticate."))
+
+      elif websocket in clients and msg.get("token") != clients[websocket]:
+        await websocket.send(notify_data(content="Unauthorized. Please authenticate."))
+      else:
+        await websocket.send(notify_data(content=f"Something wrong... {msg}"))
+
 
 async def handle_client(websocket):
   try:
